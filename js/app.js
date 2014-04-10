401be5783410fff5971827e8917dfe1642eec878
Xindong Ding <dxd.spirits@gmail.com>
2014-04-10 19:19:03 +0800

    add iscroll, add snapped scrolling to the homepage

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 $(function() {
-    $('#where>.address-wrapper').pin({
-        containerSelector : "#where",
-        minWidth : 360
+    var scroller;
+    
+    function initViews() {
+        $('.view').css('height', $('.view-wrapper').innerHeight());
+        scroller = new IScroll('.view-wrapper', {
+            momentum: false,
+            snap: true,
+            snapThreshold: 0.001,
+            snapSpeed: 500,
+            bounce: false
+        });
+    }
+    
+    /*************************************************************/
+    
+    initViews();
+    scroller.on('scrollEnd', function() {
+        if (scroller.currentPage.pageY == 1) {
+            $('#thegirl .img').removeClass('invisible');
+        } else {
+            $('#thegirl .img').addClass('invisible');
+        }
     });
-    //$(window).scroll(dropAddressCircle);
-    //showMap();
 });
