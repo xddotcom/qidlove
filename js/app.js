d47c877af17b3d9b901ae73494df99c28210b526
Xindong Ding <dxd.spirits@gmail.com>
2014-04-14 19:51:26 +0800

    load all the images before starting app

--- js/app.js
+++ js/app.js
@@ -89,23 +89,53 @@ $(function() {
     
     /*************************************************************/
     
-    $('.view').css('height', $('.view-wrapper').innerHeight());
-    scroller = new IScroll('.view-wrapper', {
-        momentum: false,
-        bounce: false,
-        snap: true,
-        snapSpeed: 500,
-        mouseWheel: true,
-        eventPassthrough: 'horizontal'
-    });
-    var sectionList = [HeroView, TheGirlView, TheBigDayView, ProposalView,
-                       GoodMorningView, LaVieView, ContactView];
-    scroller.on('scrollEnd', function() {
-        var page = scroller.currentPage.pageY;
-        sectionList[page] && sectionList[page].onEnter();
-        sectionList[page+1] && sectionList[page+1].onLeave();
-        sectionList[page-1] && sectionList[page-1].onLeave();
-    });
+    function startApp() {
+        $('.view').css('height', $('.view-wrapper').innerHeight());
+        scroller = new IScroll('.view-wrapper', {
+            momentum: false,
+            bounce: false,
+            snap: true,
+            snapSpeed: 500,
+            mouseWheel: true,
+            eventPassthrough: 'horizontal'
+        });
+        var sectionList = [HeroView, TheGirlView, TheBigDayView, ProposalView,
+                           GoodMorningView, LaVieView, ContactView];
+        scroller.on('scrollEnd', function() {
+            var page = scroller.currentPage.pageY;
+            sectionList[page] && sectionList[page].onEnter();
+            sectionList[page+1] && sectionList[page+1].onLeave();
+            sectionList[page-1] && sectionList[page-1].onLeave();
+        });
+        scroller.goToPage(0, 0);
+    }
     
-    scroller.goToPage(0, 3);
+    var imageList = [
+        "img/thegirl1.jpg", "img/thegirl2.jpg", "img/thegirl3.jpg", "img/thegirl4.jpg", "img/thegirl5.jpg",
+        "img/thestory1.jpg", "img/thestory2.jpg", "img/thestory3.jpg",
+        "img/heart-cross-pink.png",
+        "img/thecouple1.jpg", "img/thecouple2.jpg", "img/thecouple3.jpg", "img/thecouple4.jpg", "img/thecouple.png",
+        "img/story-cover.jpg", "img/story-cover-text.jpg",
+        "img/rose-bottom.jpg", "img/rose-top.jpg",
+        "img/ring.png", "img/nightsky.jpg",
+        "img/food1.jpg", "img/food2.jpg", "img/food3.jpg", "img/food4.jpg",
+        "img/cover-page-dark.jpg", "img/cover-page-light.jpg",
+        "img/bouquet.png", "img/amalfi.jpg"
+    ];
+    var l = imageList.length, i=0;
+    function imageLoaded() {
+        l--;
+        console.log(l);
+        $('.loading-text>span').text(l);
+        if (l == 0) {
+            $('.loading-text').addClass('hidden');
+            $('.view-wrapper').removeClass('hidden');
+            startApp();
+        }
+    }
+    for (i=0; i<imageList.length; i++) {
+        var image = new Image();
+        image.onload = imageLoaded;
+        image.src = imageList[i];
+    }
 });
